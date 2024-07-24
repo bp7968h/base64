3407b6674ff6944fd59efd1ea9316b31aef5cc42
Bhuwan Pandit <bp7968h@gre.ac.uk>
2024-07-24 08:36:59 +0100

    Add bae64 table

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
+const BAE64_TABLE : [char; 64] = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+];
+
+
 pub fn encode(data: &str) -> String{
+    //iterate to receive decimal value for each 
+    for (index, grapheme) in data.bytes().enumerate() {
+        
+    }
     todo!()
 }
 
 pub fn decode(data: &str) -> String {
     todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
 }
