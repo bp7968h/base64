3ccfe7e5c19cde675ed5aa0fc028eb8e3aa57df3
Bhuwan Pandit <bp7968h@gre.ac.uk>
2024-07-24 18:57:21 +0100

    Implement base64 decoding and test

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ const BASE64_TABLE : [char; 64] = [
 ];
 
 
-fn encode(data: &str) -> String {
+pub fn encode(data: &str) -> String {
     let mut binary_str = String::with_capacity(data.len() * 8);
     for byte in data.bytes(){
         binary_str.push_str(&format!("{:08b}", byte));
@@ -34,7 +34,23 @@ fn pad_to_by(uneven_str: &mut String, to: usize, by: char){
 }
 
 pub fn decode(data: &str) -> String {
-    todo!()
+    let stripped_data = data.trim_end_matches('=');
+    
+    let mut binary_str = String::new();
+    for char in stripped_data.chars(){
+        let table_index = BASE64_TABLE.iter().position(|&table_char| {
+            table_char == char
+        }).unwrap();
+        binary_str.push_str(&format!("{table_index:06b}"));
+    }
+    
+    let mut decoded_str = String::new();
+    for i in 0..(binary_str.len() / 8) {
+        let chunk_8bit = &binary_str[i*8..(i + 1) * 8];
+        let decimal_val = u32::from_str_radix(chunk_8bit, 2).unwrap();
+        decoded_str.push(char::from_u32(decimal_val).unwrap());
+    } 
+    decoded_str
 }
 
 #[cfg(test)]
@@ -76,5 +92,37 @@ mod test {
     #[test]
     fn test_encode_1_char() {
         assert_eq!(String::from("YQ=="), encode("a"))
+    }
+
+    #[test]
+    fn test_decode() {
+        assert_eq!(
+            decode("aGVsbG90aGVyZXVpamtsbw=="),
+            String::from("hellothereuijklo")
+        );
+
+        assert_eq!(decode("c3VybmFqYW4="), String::from("surnajan"));
+        assert_eq!(decode("Ym90dGwxMjEy"), String::from("bottl1212"));
+        assert_eq!(decode("QXNwaGFsdEA4OTA="), String::from("Asphalt@890"));
+        assert_eq!(
+            decode("KHNhUy5zYXNkYXcvd2Fpb25za2F3K2FzYXM9c2FkYXdheiNhc2R3"),
+            String::from("(saS.sasdaw/waionskaw+asas=sadawaz#asdw")
+        );
+        assert_eq!(
+            decode("ODkyM2prbXdlcDItPXBva21hczEuLC9hczthbHNtYWQ="),
+            String::from("8923jkmwep2-=pokmas1.,/as;alsmad")
+        );
+        assert_eq!(
+            decode("XHBvcG9rZXdubXBjITJrc21kU2xta3Ns"),
+            String::from("\\popokewnmpc!2ksmdSlmksl")
+        );
+    }
+    
+     #[test]
+    fn test_longer_decoding() {
+        assert_eq!(
+            decode("VVBEQ3VGNW0yRlBQUGdaeXh6YVdGWXYxMG5UVWYyM1M1OFU1a0RDeWRYRjRyUDdBN1dFUGdROUt3akc1WDA2VGo2MXQ3Uk5HNFRDWEpYR0JrVm1pSGhack91RmNnVDR4TDRBc0R4d3dtd2N2bHlKU1RqUXZkem43MHd1SXhYZVVBbnI5bUw4TjhyalBnWG5CakRNZWNhZUUxOElWbXcyZ3F1MjFHaDR1N0ZZeHcwSG1SQUtRcFFQWFNGNzVka2Jjb3NVT1AzT1VNVjd6ZFNxYTVhckRDM2pUT3F4N0llaVc1TzNIa2hqblo0dVdJeElVUnZOaVppbDZZNXNCUTJUSkpldldQN1dIaVdOWEprYU92WU9qMjg0Z0NHeXptYlZJN3ZvSnM0eVdLZUdNdWhSYk9Sczg5aEk2dmlVY25ZNmoyVHE5Q2UyYlNLMlZtRkxQaVd0bzdFQ0IzYzBDeFZsbUtaVGtyOHRtMUpUT2hadVltOXlVbkQxdHRzc3ZSemcwZVhFMVdjREZjUmZ1dEZINENHeEdqYU9sUTJxaktkTElaQ0ZBejcxNnJMa096SkdHSDRrRXVza2UyVDhPT3R4Y2xWZ3JHcktNMTh5YXJiS0xBalBYYkJudWlHVTRUUFMxUDkzZHJyRExIbklJTlpDeTExdXJyNHFEMWNhODkxZUlzZ1FFQ3F6d0piY1NiTFlFMTVZNzhqS1VtdmYwQ2RpNW1ZTTM0MlVKU3QyVjZQSHRybkFmZXVvT1o2ZGtKNjlrV014ck10ZldES0VzZ01jNHlLU1hudEpIZHhpbFpYVFNwZk5Lb0hLWk9IbWRKSEFuRVlVWUNnTk9waExiQ2NRNHNKazQ3aWpGTWZRWklPV1Q2Y1NDdHI1VzlHS0dBb1BPb2dPQnczMmFDektJbjR4dmhxREdPZ3ZMcTUwR3Y4QzdKYk1ZODdEMHdtTGNhS2FON1JCbFU1bGRUWDFqdmlGNGZmV21IQ2Y1VkhMRzJXbjZWS0xyMXBqVlFwdExiZlZITGxoTjd1WTR1WkdjSmRuNm1ZTmtmTWhPSWFRWHJ2d0tTaFU1RkZucTlzZHI5bnBCYUNaSU4xNEM0bFpId09KUXoySzl6OHJtSkRZSlQ4R0RLVFNLeDBVNmJaR0ZBM3VzN0dxelBoc0VKUXhpWktIbVRlekpQVTRPUlAwYm5TUzgwd2FaSlR4V3Zic0hUeXEyTjQ0RjZYM0lhcTMwaHdpVGtBTDdoUWRoWVBYUGEybXl2Z0lvNE16UWtwUFBPTms2bzlOM3lCUjA1TVk0VXZPbmdOZ0R2RUZXZ2xaMlpwZmpoQ1lhZnFxdVRv"),
+            String::from("UPDCuF5m2FPPPgZyxzaWFYv10nTUf23S58U5kDCydXF4rP7A7WEPgQ9KwjG5X06Tj61t7RNG4TCXJXGBkVmiHhZrOuFcgT4xL4AsDxwwmwcvlyJSTjQvdzn70wuIxXeUAnr9mL8N8rjPgXnBjDMecaeE18IVmw2gqu21Gh4u7FYxw0HmRAKQpQPXSF75dkbcosUOP3OUMV7zdSqa5arDC3jTOqx7IeiW5O3HkhjnZ4uWIxIURvNiZil6Y5sBQ2TJJevWP7WHiWNXJkaOvYOj284gCGyzmbVI7voJs4yWKeGMuhRbORs89hI6viUcnY6j2Tq9Ce2bSK2VmFLPiWto7ECB3c0CxVlmKZTkr8tm1JTOhZuYm9yUnD1ttssvRzg0eXE1WcDFcRfutFH4CGxGjaOlQ2qjKdLIZCFAz716rLkOzJGGH4kEuske2T8OOtxclVgrGrKM18yarbKLAjPXbBnuiGU4TPS1P93drrDLHnIINZCy11urr4qD1ca891eIsgQECqzwJbcSbLYE15Y78jKUmvf0Cdi5mYM342UJSt2V6PHtrnAfeuoOZ6dkJ69kWMxrMtfWDKEsgMc4yKSXntJHdxilZXTSpfNKoHKZOHmdJHAnEYUYCgNOphLbCcQ4sJk47ijFMfQZIOWT6cSCtr5W9GKGAoPOogOBw32aCzKIn4xvhqDGOgvLq50Gv8C7JbMY87D0wmLcaKaN7RBlU5ldTX1jviF4ffWmHCf5VHLG2Wn6VKLr1pjVQptLbfVHLlhN7uY4uZGcJdn6mYNkfMhOIaQXrvwKShU5FFnq9sdr9npBaCZIN14C4lZHwOJQz2K9z8rmJDYJT8GDKTSKx0U6bZGFA3us7GqzPhsEJQxiZKHmTezJPU4ORP0bnSS80waZJTxWvbsHTyq2N44F6X3Iaq30hwiTkAL7hQdhYPXPa2myvgIo4MzQkpPPONk6o9N3yBR05MY4UvOngNgDvEFWglZ2ZpfjhCYafqquTo")
+        );
     }
 }
